bae2b57b914ca84ac2bbeac142a48961ada7d8c1
Bee <bhargavibattala@gmail.com>
2020-09-24 18:56:11 +1000

    small updates before pulling code

--- regular-expressions/test-method.js
+++ regular-expressions/test-method.js
@@ -3,3 +3,13 @@
    let testStr = "freeCodeCamp";
    let testRegex = /Code/;
    testRegex.test(testStr); // Returns true
+
+   // You can search for multiple patterns using the alternation or OR operator: |.
+   let petString = "James has a pet cat.";
+   let petRegex = /dog|cat|bird|fish/; // Change this line
+   let result = petRegex.test(petString);
+
+   // Ignore case while matching
+   let myString = "freeCodeCamp";
+let fccRegex = /FreeCodeCamp/i; // 'i' flags that the system needs to ignore the case
+let result = fccRegex.test(myString);

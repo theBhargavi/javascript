eb321da16b516f353d2065c1b7e19f657eb73d86
Bee <bhargavibattala@gmail.com>
2020-09-24 18:40:09 +1000

    added regular expressions folder

---
+++ regular-expressions/test-method.js
@@ -0,0 +1,5 @@
+/* Regular expressions are used in programming languages to match parts of strings. 
+   You create patterns to help you do that matching. */
+   let testStr = "freeCodeCamp";
+   let testRegex = /Code/;
+   testRegex.test(testStr); // Returns true

189129142cbef8232a91ba3037306d5cac9dedc2
Bee <bhargavibattala@gmail.com>
2020-09-27 22:32:07 +1000

    final commit for regex

--- regular-expressions/test-method.js
+++ regular-expressions/test-method.js
@@ -56,3 +56,12 @@
     firstRegex.test(firstString); // Returns true
     let notFirst = "You can't find Ricky now.";
     firstRegex.test(notFirst); // Returns false
+
+    let longHand = /[A-Za-z0-9_]+/;
+   let shortHand = /\w+/;
+   let numbers = "42";
+   let varNames = "important_var";
+   longHand.test(numbers); // Returns true
+   shortHand.test(numbers); // Returns true
+   longHand.test(varNames); // Returns true
+   shortHand.test(varNames); // Returns true
